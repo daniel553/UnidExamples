6344ed6944977026f3416b8070bbfc44188d46e8
Daniel <dany.el553@gmail.com>
2017-11-16 18:49:51 -0600

    Added hello worl

---
+++ HolaMundoUnid/app/src/main/java/edu/unid/lsi/holamundounid/NuevaActivity.java
@@ -0,0 +1,13 @@
+package edu.unid.lsi.holamundounid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NuevaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nueva);
+    }
+}
